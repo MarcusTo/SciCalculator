8f0ae72ccc5809b928a09b8bd74cf422db43c85f
MarcusTo <toman.markus12@gmail.com>
2023-03-15 15:34:24 +0200

    Calculator done

--- SciCalculator/SciCalculator/Views/CalculatorPage.xaml.cs
+++ SciCalculator/SciCalculator/Views/CalculatorPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Maui.Controls;
+using SciCalculator.ViewModels;
 
 namespace SciCalculator.Views;
 
@@ -7,5 +8,8 @@ public partial class CalculatorPage : ContentPage
     public CalculatorPage()
     {
         InitializeComponent();
+
+        //Init the view model
+        this.BindingContext = new CalculatorPageViewModel();
     }
 }
